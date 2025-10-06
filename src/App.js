42e9d0ad8a75ac39dc733a43d5fcbeb215fbcab1
Aadhit <aadhitviji7@gmail.com>
2025-10-06 12:01:15 +0530

    Refactor App component to remove boilerplate and display a centered welcome message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React zzz
-        </a>
-      </header>
-    </div>
-  );
+    const headingStyle = {
+        color: 'green',
+        textAlign: 'center'
+    };
+
+    return <h1 style={headingStyle}>Welcome</h1>;
 }
 
 export default App;
